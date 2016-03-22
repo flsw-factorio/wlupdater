0427070a5b3799f5ee6b9f4dae09ecc76aeb6126
Can Aydin <ca@fluytsoftware.com>
2016-03-22 11:13:48 +0200

    do whitelisting and blocking

--- wlupdater.py
+++ wlupdater.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python
 
-import sys
+import sys, subprocess
 import BaseHTTPServer
 import SocketServer
 
 PORT = 8080
 
-if len(sys.argv) < 2:
-    exit("Error: have to provide destination file")
+if len(sys.argv) < 3:
+    exit("Error: have to provide destination file and paths to whitelist and block scripts")
 
-FILEPATH = sys.argv[1]
+WL_TXT = sys.argv[1]
+WL_SH = sys.argv[2]
+BLOCK_SH = sys.argv[3]
 
-if len(sys.argv) > 2:
-    PORT = int(sys.argv[2])
+if len(sys.argv) > 4:
+    PORT = int(sys.argv[4])
 
 class WLHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_HEAD(self):
@@ -29,27 +31,33 @@ class WLHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.wfile.write("</body></html>")
     def do_PUT(self):
         request_path = self.path
-
-        print("\n----- Request Start ----->\n")
-        print(request_path)
-
         request_headers = self.headers
         content_length = request_headers.getheaders('content-length')
         length = int(content_length[0]) if content_length else 0
-        whitelist = self.rfile.read(length)
+        data = self.rfile.read(length)
+
+        print("\n----- Handling Request ----->\n")
+        print(request_path)
 
         print(request_headers)
-        with open(FILEPATH, "w") as f:
-            f.write(whitelist)
+        if request_path == "/whitelist.txt":
+            print("New whitelist: \n", data)
+            with open(WL_TXT, "w") as f:
+                f.write(data)
+            subprocess.call(['sudo', WL_SH])
 
-        print(whitelist)
-        print("<----- Request End -----\n")
+        if request_path == "/block":
+            print("Blocking: ", data)
+            subprocess.call(['sudo', BLOCK_SH, data])
+
+
+        print("<----- Request Finished -----\n")
 
         self.send_response(200)
 
 
 
-print "Serving at port", PORT, "to", FILEPATH
+print "Serving at port", PORT
 
 httpd = SocketServer.TCPServer(("", PORT), WLHandler)
 httpd.serve_forever()
